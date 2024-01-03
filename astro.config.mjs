347213510739a37a7567e8707d06ee4c0c8e32be
jamerrq <jamerrq@gmail.com>
2024-01-03 18:02:14 -0500

    🚀 feat: deno SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config'
-
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import deno from '@astrojs/deno';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-})
+  integrations: [tailwind()],
+  adapter: deno(),
+  output: 'server'
+});
