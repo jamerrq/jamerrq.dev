a32c487616ecc95625db86390c0b3c90494e01c7
jamerrq <jamerrq@gmail.com>
2024-01-04 20:01:53 -0500

    🔨 setting up eslint & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  plugins: ['jsx-a11y'],
+  extends: ['plugin:astro/recommended', 'plugin:jsx-a11y/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+    ecmaVersion: 'latest'
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      }
+    }
+  ]
+}
---
+++ prettier.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  // i am just using the standard config, change if you need something else
+  ...require('prettier-config-standard'),
+  plugins: [require.resolve('prettier-plugin-astro')],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
+}
