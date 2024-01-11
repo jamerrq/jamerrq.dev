e4f75300108681775ffe35854fee4361f1a47c38
jamerrq <jamerrq@gmail.com>
2024-01-11 17:32:48 -0500

    🚩 improved styles, started i18n routing, modularize some code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import deno from '@astrojs/deno';
+import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+import deno from '@astrojs/deno'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   adapter: deno(),
-  output: 'server'
-});
+  output: 'server',
+  i18n: {
+    locales: ['en', 'es'],
+    defaultLocale: 'en',
+    fallback: {
+      'es': 'en'
+    }
+  }
+})
