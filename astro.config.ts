08e218d8b6107e6824c54b267fdcff3f98e1e9de
jamerrq <jamerrq@gmail.com>
2024-03-19 16:07:34 -0500

    feat: optimizations

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import deno from '@astrojs/deno';
-import robotsTxt from 'astro-robots-txt';
-import preact from '@astrojs/preact';
-import pageInsight from 'astro-page-insight';
-import sitemap from '@astrojs/sitemap';
-import compress from 'astro-compress';
-import { VitePWA } from 'vite-plugin-pwa';
-import { manifest } from './src/utils/seoConfig';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import deno from '@astrojs/deno'
+import robotsTxt from 'astro-robots-txt'
+import preact from '@astrojs/preact'
+import pageInsight from 'astro-page-insight'
+import sitemap from '@astrojs/sitemap'
+import compress from 'astro-compress'
+import { VitePWA } from 'vite-plugin-pwa'
+import { manifest } from './src/utils/seoConfig'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,9 +19,9 @@ export default defineConfig({
     preact(),
     pageInsight(),
     sitemap({
-      filter: page =>
-      page !== 'https://jamerrq.deno.dev/board/' &&
-      page !== 'https://jamerrq.deno.dev/es/board/'
+      filter: (page) =>
+        page !== 'https://jamerrq.deno.dev/board/' &&
+        page !== 'https://jamerrq.deno.dev/es/board/'
     }),
     mdx(),
     compress()
@@ -43,16 +43,20 @@ export default defineConfig({
     // ssr: {
     //   external:[]
     // },
-    plugins: [VitePWA({
-      registerType: 'autoUpdate',
-      manifest,
-      workbox: {
-        globDirectory: 'dist/client',
-        globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
-        // Don't fallback on document based (e.g. `/some-page`) requests
-        // This removes an errant console.log message from showing up.
-        navigateFallback: null
-      }
-    })]
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest,
+        workbox: {
+          globDirectory: 'dist/client',
+          globPatterns: [
+            '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'
+          ],
+          // Don't fallback on document based (e.g. `/some-page`) requests
+          // This removes an errant console.log message from showing up.
+          navigateFallback: null
+        }
+      })
+    ]
   }
-});
+})
